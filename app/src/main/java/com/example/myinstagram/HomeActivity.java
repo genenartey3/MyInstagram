936af4c79092131fde1f001fd61a9a3d25ebb526
Gene Nartey <genenartey@fb.com>
2019-07-10 17:05:01 -0700

    Added navbar with nessesary fragments attached. Next is to implement posts

--- app/src/main/java/com/example/myinstagram/HomeActivity.java
+++ app/src/main/java/com/example/myinstagram/HomeActivity.java
@@ -1,90 +1,74 @@
 package com.example.myinstagram;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.example.myinstagram.Fragments.ComposeFragment;
+import com.example.myinstagram.Fragments.FeedFragment;
+import com.example.myinstagram.Fragments.ProfileFragment;
 import com.example.myinstagram.model.Post;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
-import java.io.File;
 import java.util.List;
 
 
 public class HomeActivity extends AppCompatActivity {
 
-    // initialize layout variables
-    private EditText descriptionInput;
-    private Button btnCreate;
-    private Button btnRefresh;
-    private Button btnGetPic;
+
+
+    //initialize Navigation Bar variable
+    private BottomNavigationView bottomNavigationView;
 
 
     // image path will be deleted later
     private static String imagePath = "https://pbs.twimg.com/profile_images/1111508562000400385/D1LwKGUB_400x400.jpg";
 
 
-    //variables needed for camera
-    public final String APP_TAG = "MyCustomApp";
-    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
-    public String photoFileName = "photo.jpg";
-    File photoFile;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        // establish references to layout elements
-        descriptionInput = findViewById(R.id.etDescription);
-        btnCreate = findViewById(R.id.btnCreate);
-        btnRefresh = findViewById(R.id.btnRefresh);
-        btnGetPic = findViewById(R.id.btnGetPic);
+        //create fragment manager
+        final FragmentManager fragmentManager = getSupportFragmentManager();
 
-        // register Post class as subclass
-        ParseObject.registerSubclass(Post.class);
+        //reference to bottom nav bar layout
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        // setting on click listener to fire an intent to open camera
-        btnCreate.setOnClickListener(new View.OnClickListener() {
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                // get description from edit text file, get current user as well
-                final String description = descriptionInput.getText().toString();
-                final ParseUser user = ParseUser.getCurrentUser();
-
-                // uncomment when finished with image capture
-                //final File file = new File(imagePath);
-                //ParseFile parseFile = new ParseFile(file);
-
-                createPost(description, user);
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                 Fragment fragment = null;
+                 switch  (menuItem.getItemId()) {
+                     case R.id.action_compose :
+                         fragment = new ComposeFragment();
+                         Toast.makeText(HomeActivity.this, "Compose!", Toast.LENGTH_LONG).show();
+                         break;
+                     case R.id.action_profile :
+                         fragment = new ProfileFragment();
+                         Toast.makeText(HomeActivity.this, "Profile !", Toast.LENGTH_LONG).show();
+                         break;
+                     case R.id.action_home:
+                         fragment = new FeedFragment();
+                         Toast.makeText(HomeActivity.this, "Home!", Toast.LENGTH_LONG).show();
+                         break;
+                 }
+                 fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                 return true;
             }
         });
-
-        btnRefresh.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadTopPosts();
-            }
-        });
-
-        btnCreate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 
     private void loadTopPosts() {
@@ -116,68 +100,5 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
-    private void createPost(String description, ParseUser user) {
-        // create new Post object
-        final Post newPost = new Post();
-        Log.d("CreatePost", "YEeerrrrRRR");
 
-        // insert post data into Post object
-        newPost.setDescription(description);
-        // newPost.setImage(imageFile);
-        newPost.setUser(user);
-
-        newPost.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e == null ) {
-                     Log.d("HomeActivity", "Create Post Success!!");
-                     descriptionInput.setText("");
-                } else {
-                    Log.d("HomeActivity", "Failed to create post");
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-    // Returns the File for a photo stored on disk given the fileName
-    public File getPhotoFileUri(String fileName) {
-        // Get safe storage directory for photos
-        // Use `getExternalFilesDir` on Context to access package-specific directories.
-        // This way, we don't need to request external read/write runtime permissions.
-        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
-
-        // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
-            Log.d(APP_TAG, "failed to create directory");
-        }
-
-        // Return the file target for the photo based on filename
-        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
-
-        return file;
-    }
-
-
-
-
-    public void onLaunchCamera(View view) {
-        // create Intent to take a picture and return control to the calling application
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Create a File reference to access to future access
-        photoFile = getPhotoFileUri(photoFileName);
-
-        // wrap File object into a content provider
-        // required for API >= 24
-        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
-        Uri fileProvider = FileProvider.getUriForFile(HomeActivity.this, "com.codepath.fileprovider", photoFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            // Start the image capture intent to take photo
-            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-        }
-    }
 }
--- app/src/main/java/com/example/myinstagram/MainActivity.java
+++ app/src/main/java/com/example/myinstagram/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myinstagram;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -11,6 +13,7 @@ import android.widget.EditText;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,16 +21,26 @@ public class MainActivity extends AppCompatActivity {
     private EditText usernameInput;
     private EditText passwordInput;
     private Button btnLogin;
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //start login activity animation
+        ConstraintLayout constraintLayout = findViewById(R.id.mainlayout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+
+
         // establish references to layout variables
         usernameInput = findViewById(R.id.etUsername);
         passwordInput = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +50,47 @@ public class MainActivity extends AppCompatActivity {
                 final String password  = passwordInput.getText().toString();
 
                 login(username, password);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get string input from reference
+                final String newUsername = usernameInput.getText().toString();
+                final String password = passwordInput.getText().toString();
+
+                // fire new method to create new user in parse database
+                createNewUser(newUsername, password);
+            }
+        });
+
+
+
+    }
+
+    private void createNewUser(String newUsername, String password) {
+        // create new user
+        ParseUser user = new ParseUser();
+
+        // set username to newUsername String and password to password String
+        user.setUsername(newUsername);
+        user.setPassword(password);
+        user.setEmail(newUsername + "@gmail.com");
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if ( e == null ) {
+                    Log.d("MainActivity","SignUp Success!!");
+
+                    Intent toHome = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(toHome);
+                    finish();
+                } else {
+                    e.printStackTrace();
+                    Log.d("MainActivity", "SignUp Failure :( ");
+                }
             }
         });
 
@@ -55,6 +109,8 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(toHome);
                     finish();
                 } else {
+                    usernameInput.setText("");
+                    passwordInput.setText("");
                     Log.d("LoginActivity", "Login Failed");
                     e.printStackTrace();
                 }
@@ -62,4 +118,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
 }
